1c8b077a8d2494107cdd37e0f321f6fe62b0a2fa
wwj <wuwenjie718@gmail.com>
2017-02-18 18:37:10 +0800

    添加个人信息

--- formbuilder/components/Header.js
+++ formbuilder/components/Header.js
@@ -11,9 +11,10 @@ export default function Header(props) {
       </div>
       <div className="navbar-collapse collapse">
         <ul className="nav navbar-nav navbar-right">
-          <li><Link to="/faq">FAQ</Link></li>
-          <li><a href="https://kinto.readthedocs.io/en/latest/overview.html">我们的价值观</a></li>
-          <li><a href="https://github.com/wwj718/formbuilder_client">SDK</a></li>
+          <li><Link target="_blank" to="/faq">FAQ</Link></li>
+          <li><a target="_blank" href="https://kinto.readthedocs.io/en/latest/overview.html">我们的价值观</a></li>
+          <li><a target="_blank" href="https://github.com/wwj718/formbuilder_client">SDK</a></li>
+          <li><a target="_blank" href="http://blog.just4fun.site">我的博客</a></li>
         </ul>
       </div>
     </div>
